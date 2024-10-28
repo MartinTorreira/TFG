7d82675d2f86d9613e489b853e299c76729fe0e9
Martín Torreira Portos <m.tportos@udc.es>
2024-10-28 18:38:50 +0100

    Some styles on chat dropdown

--- frontend/src/components/chat/ChatPage.jsx
+++ frontend/src/components/chat/ChatPage.jsx
@@ -21,8 +21,7 @@ const ChatPage = ({ setSelectedConversationId, selectedConversationId }) => {
     loadInitialMessages,
     sendMessage,
   } = useChatStore();
-  const [receiverDetails, setReceiverDetails] = useState(null);
-  const [senderDetails, setSenderDetails] = useState(null);
+  const [userDetails, setUserDetails] = useState({});
   const [isAnimating, setIsAnimating] = useState(false);
   const [timeNow, setTimeNow] = useState(dayjs());
 
@@ -62,15 +61,43 @@ const ChatPage = ({ setSelectedConversationId, selectedConversationId }) => {
   }, [addMessageToConversation]);
 
   useEffect(() => {
+    const fetchDetails = async () => {
+      const userIds = Object.keys(conversations).flatMap((conversationId) =>
+        conversationId.split("-").filter((id) => id !== user.id.toString()),
+      );
+      const uniqueUserIds = [...new Set(userIds)];
+      const detailsPromises = uniqueUserIds.map(
+        (userId) =>
+          new Promise((resolve, reject) => {
+            getUserById(userId, resolve, reject);
+          }),
+      );
+      const details = await Promise.all(detailsPromises);
+      const detailsMap = details.reduce((acc, detail) => {
+        acc[detail.id] = detail;
+        return acc;
+      }, {});
+      setUserDetails(detailsMap);
+    };
+
+    if (Object.keys(conversations).length > 0) {
+      fetchDetails();
+    }
+  }, [conversations, user.id]);
+
+  useEffect(() => {
     if (selectedConversationId) {
       const [userId1, userId2] = selectedConversationId.split("-");
       loadMessages(userId1, userId2);
-      const otherUserId = userId1 === user.id.toString() ? userId2 : userId1;
-      getUserById(otherUserId, setReceiverDetails, (error) =>
-        console.error("Error fetching user:", error),
-      );
-      getUserById(user.id, setSenderDetails, (error) =>
-        console.error("Error fetching user:", error),
+      getUserById(
+        user.id,
+        (data) => {
+          setUserDetails((prevDetails) => ({
+            ...prevDetails,
+            [user.id]: data,
+          }));
+        },
+        (error) => console.error("Error fetching user:", error),
       );
     }
   }, [selectedConversationId, user.id, loadMessages]);
@@ -117,21 +144,62 @@ const ChatPage = ({ setSelectedConversationId, selectedConversationId }) => {
 
   return (
     <div className={`flex h-[600px] ${isAnimating ? "slide-up" : ""}`}>
-      {" "}
-      {/* Aplica la clase CSS */}
-      <div className="w-1/4 p-4 bg-gray-100 h-full overflow-y-auto">
+      <div className="w-1/3 p-4 bg-gray-100 h-full overflow-y-auto">
         <h2 className="text-lg font-semibold mb-4">Chats</h2>
-        {Object.keys(conversations).map((conversationId) => (
-          <div
-            key={conversationId}
-            className="cursor-pointer hover:bg-gray-200 p-2 mt-2 rounded"
-            onClick={() => handleConversationClick(conversationId)}
-          >
-            <p className="font-medium">{conversationId}</p>
-          </div>
-        ))}
+        {Object.keys(conversations).map((conversationId) => {
+          const [userId1, userId2] = conversationId.split("-");
+          const otherUserId =
+            userId1 === user.id.toString() ? userId2 : userId1;
+          const lastMessage =
+            conversations[conversationId].messages.slice(-1)[0];
+          const userDetail = userDetails[otherUserId];
+
+          return (
+            <div
+              key={conversationId}
+              className="flex flex-row cursor-pointer hover:bg-gray-200 p-2 mt-2 -ml-2 border-y"
+              onClick={() => handleConversationClick(conversationId)}
+            >
+              <img
+                src={userDetail?.avatar}
+                alt="Receiver Avatar"
+                className="w-10 h-10 rounded-full"
+              />
+              <div className="flex flex-col items-center space-x-2">
+                <p className="font-medium">{userDetail?.userName}</p>
+                <p className="text-sm text-gray-600">
+                  {lastMessage?.content || "No messages"}
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <div className="flex flex-col items-center justify-center w-3/4 bg-white shadow-xl rounded-lg overflow-hidden h-full">
+      <div className="flex flex-col items-center justify-center w-3/4 bg-white overflow-hidden h-full">
+        {selectedConversationId && (
+          <div className="w-full p-4 bg-gray-200 flex items-center">
+            <img
+              src={
+                userDetails[
+                  selectedConversationId
+                    .split("-")
+                    .find((id) => id !== user.id.toString())
+                ]?.avatar || ""
+              }
+              alt="Receiver Avatar"
+              className="w-10 h-10 rounded-full mr-4"
+            />
+            <p className="font-medium">
+              {
+                userDetails[
+                  selectedConversationId
+                    .split("-")
+                    .find((id) => id !== user.id.toString())
+                ]?.userName
+              }
+            </p>
+          </div>
+        )}
         <div className="flex flex-col flex-grow w-full p-4 overflow-auto">
           {selectedConversationId && conversations[selectedConversationId] ? (
             conversations[selectedConversationId].messages.map((msg, index) => (
@@ -139,16 +207,9 @@ const ChatPage = ({ setSelectedConversationId, selectedConversationId }) => {
                 key={index}
                 className={`flex w-full mt-2 space-x-3 max-w-xs ${msg.senderId === user.id ? "ml-auto justify-end" : ""}`}
               >
-                {msg.senderId !== user.id && (
-                  <img
-                    className="flex-shrink-0 h-10 w-10 rounded-full"
-                    src={receiverDetails?.avatar}
-                    alt="Receiver Avatar"
-                  />
-                )}
                 <div>
                   <div
-                    className={`p-3 rounded-lg ${msg.senderId === user.id ? "bg-accent-dark text-white rounded-l-lg rounded-br-lg" : "bg-gray-300 rounded-r-lg rounded-bl-lg"}`}
+                    className={`p-3  ${msg.senderId === user.id ? "bg-accent-dark text-white rounded-l-lg rounded-br-xl" : "bg-gray-300 rounded-r-lg rounded-bl-xl"}`}
                   >
                     <p className="text-sm">{msg.content}</p>
                   </div>
@@ -156,13 +217,6 @@ const ChatPage = ({ setSelectedConversationId, selectedConversationId }) => {
                     {getTimeDifference(msg.timestamp)}
                   </span>
                 </div>
-                {msg.senderId === user.id && (
-                  <img
-                    className="flex-shrink-0 h-10 w-10 rounded-full"
-                    src={senderDetails?.avatar}
-                    alt="Receiver Avatar"
-                  />
-                )}
               </div>
             ))
           ) : (
@@ -170,9 +224,9 @@ const ChatPage = ({ setSelectedConversationId, selectedConversationId }) => {
           )}
           <div ref={messagesEndRef} />
         </div>
-        <div className="bg-gray-300 p-4 flex flex-row space-x-2 w-full">
+        <div className="bg-gray-100 p-2 px-0 mr-2 flex flex-row space-x-2 w-full">
           <input
-            className="flex items-center h-10 w-full rounded px-3 text-sm"
+            className="flex items-center h-10 w-full rounded px-3 text-sm focus:ring-0"
             type="text"
             placeholder="Escribe un mensaje…"
             value={message}
